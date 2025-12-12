d7a0adb82a33e360ffd943eaf0e43ff5ccb07ff6
2kfarid3 <fkarimli047@gmail.com>
2025-12-12 21:46:30 +0900

    First Initialization - Database Connection

---
+++ src/test/java/inha/edu/kr/airlineproject/AirlineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package inha.edu.kr.airlineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
